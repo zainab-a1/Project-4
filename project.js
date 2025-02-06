9184d918f7ba2f0b1b5bf2e4aa3578e6012aa7da
zainab-a1 <sagalawil@icloud.com>
2025-02-06 18:24:36 +0000

    Added new files

---
+++ project.js
@@ -0,0 +1,76 @@
+// Variables
+
+const addTask = document.getElementById('add-task');
+const taskConatiner = document.getElementById('task-container');
+const inputTask = document.getElementById('input-task');
+
+// EVENT LISTENER FOR ADD BUTTON
+
+addTask.addEventListener('click', function(){
+
+    let task = document.createElement('div');
+    task.classList.add('task');
+
+
+    let li = document.createElement('li');
+    li.innerText = inputTask.value;
+    task.appendChild(li);
+
+    let checkButton = document.createElement("button");
+    checkButton.innerHTML = '<i class="fa-solid fa-check">';
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+
+    let deleteButton= document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    let editButton = document.createElement("button");
+    editButton.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+    editButton.classList.add('editTask');
+    task.appendChild(editButton);
+
+
+    if(inputTask.value === "") {
+        alert("Please enter a task");
+    }else {
+        taskConatiner.appendChild(task);
+    }
+
+    inputTask.value = "";
+
+
+    checkButton.addEventListener('click', function(){
+
+
+        if (checkButton.parentElement.style.textDecoration === "line-through") {
+            checkButton.parentElement.style.textDecoration = "";
+  
+        } else {
+            checkButton.parentElement.style.textDecoration = "line-through";
+
+
+        }
+
+    });
+
+
+    deleteButton.addEventListener('click', function(e){
+
+        let target = e.target;
+
+        task.remove();
+
+        console.log(e);
+
+    });
+
+    editButton.addEventListener('click', function(e) {
+
+        let target = e.target;
+        task.edit();
+
+    })
+});
